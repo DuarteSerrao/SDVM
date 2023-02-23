0a2f0cb080eedb4ce06abbd2c933f14999480ca6
Duarte Serrão <duarte.serrao.1999@gmail.com>
2023-02-23 10:46:25 +0000

    quicksort

--- RAPL/C/quicksort.c
+++ RAPL/C/quicksort.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 
-void quicksort(int number[25],int first,int last){
+
+int count = 128;
+int list[count] = {123, 45, 78, 34, 102, 121, 37, 49, 36, 12, 40, 76, 65, 44, 111, 128, 3, 69, 118, 35, 125, 112, 88, 20, 92, 53, 30, 90, 29, 124, 115, 54, 95, 70, 97, 122, 99, 113, 105, 110, 75, 50, 127, 82, 19, 22, 109, 33, 6, 31, 85, 87, 83, 64, 24, 93, 42, 91, 4, 98, 116, 25, 117, 107, 126, 10, 77, 46, 15, 59, 48, 60, 2, 17, 86, 58, 80, 51, 119, 61, 9, 56, 74, 43, 14, 89, 11, 71, 23, 100, 114, 96, 84, 7, 27, 79, 1, 72, 28, 16, 108, 106, 41, 52, 32, 47, 73, 62, 66, 104, 18, 8, 101, 13, 39, 55, 57, 120, 68, 5, 103, 94, 26, 38, 21, 67, 63, 81}
+
+void quicksort(int number[count],int first,int last){
     int i, j, pivot, temp;
 
     if(first<last){
@@ -29,20 +33,16 @@ void quicksort(int number[25],int first,int last){
    }
 }
 
-int main(int argc, char** argv){
-    int i, count;
-    int *list = NULL;
-    
-    if (argc < 2 ) return -1; // nome ficheiro, array
-
-
-    count = argc - 1;
-    list = (int *)malloc(count * sizeof(int));
-
-    for(i=0;i<count;i++)
-       list[i] = atoi(argv[i+1]);
+int main(){
+    int i;
  
     quicksort(list,0,count-1);
- 
+    
+    printf("\nSorted list:\n"); // Display the sorted array
+    for (i = 0; i < count; i++)
+    {
+        printf("%d ", list[i]);// we're using C , so demonstrate pointers :) 
+    }
+
     return 0;
 }
