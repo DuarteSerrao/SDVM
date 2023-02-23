fffc39beab82b0f0e0fa192e21ff9c679e98b7a3
Duarte Serrão <duarte.serrao.1999@gmail.com>
2023-02-23 09:59:44 +0000

    quicksort recebe como entrada coisas

--- RAPL/C/quicksort.c
+++ RAPL/C/quicksort.c
@@ -33,11 +33,14 @@ int main(int argc, char** argv){
     int i, count;
     int *list = NULL;
     
-    if (argc < 2 || argc < (2+argv[1])) return -1; // nome ficheiro, tamanho, array
+    if (argc < 2 || argc < (2+atoi(argv[1]))) return -1; // nome ficheiro, tamanho, array
 
 
-    count = argv[1];
-    list = (int *)malloc(n * sizeof(int));
+    count = atoi(argv[1]);
+    list = (int *)malloc(count * sizeof(int));
+
+    for(i=0;i<count;i++)
+       list[i] = atoi(argv[i+2]);
  
     quicksort(list,0,count-1);
  
